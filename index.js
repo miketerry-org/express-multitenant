01b49796132389e89c4599f9916974fa73016e87
Mike Terry <support@miketerry.org>
2025-02-22 19:27:27 -0600

    changed package to only export Teants class

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 "use strict";
 
 // load all necessary modules
-const Tenant = require("./lib/tenant");
-const tenants = require("./lib/tenants");
+const Tenants = require("./lib/tenants");
 
-module.exports = { Tenant, tenants };
+module.exports = Tenants
