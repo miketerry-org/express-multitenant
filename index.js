cb564c9cf4e3630bd3abc5139a9774237643ccc2
Mike Terry <support@miketerry.org>
2025-04-04 12:52:09 -0500

    created services for winston+mongoDB logging and mongoose database

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
 "use strict";
 
 // load all necessary modules
-const Service = require("./lib/service");
 const Tenant = require("./lib/tenant");
 const Tenants = require("./lib/tenants");
+const Service = require("./lib/service");
+const mongooseService = require("./lib/mongooseService");
 const NodemailerService = require("./lib/nodemailerService");
+const winstonMongoDBService = require("./lib/winstonMongoDBService");
 
-// export the tenant, tenants and nodemailer classes
-module.exports = { Tenant, Tenants, Service, NodemailerService };
+// export the tenant, tenants and pre-defined service classes
+module.exports = {
+  Tenant,
+  Tenants,
+  Service,
+  mongooseService,
+  NodemailerService,
+  winstonMongoDBService,
+};
