a3760028b9367923da898e7215fa79d801edf62c
Mike Terry <support@miketerry.org>
2025-04-07 17:38:07 -0500

    replaced service classes with service create functions

--- index.js
+++ index.js
@@ -5,17 +5,16 @@
 // load all necessary modules
 const Tenant = require("./lib/tenant");
 const Tenants = require("./lib/tenants");
-const Service = require("./lib/service");
-const mongooseService = require("./lib/mongooseService");
-const NodemailerService = require("./lib/nodemailerService");
-const winstonMongoDBService = require("./lib/winstonMongoDBService");
+const { setServerLog } = require("./lib/serverLog");
+const createMongoDBConnection = require("./lib/createMongoDBConnection");
+const createNodeMailer = require("./lib/createNodeMailer");
+const createWinstonMongoDBLog = require("./lib/createWinstonMongoDBLog");
 
-// export the tenant, tenants and pre-defined service classes
 module.exports = {
   Tenant,
   Tenants,
-  Service,
-  mongooseService,
-  NodemailerService,
-  winstonMongoDBService,
+  setServerLog,
+  createMongoDBConnection,
+  createNodeMailer,
+  createWinstonMongoDBLog,
 };
